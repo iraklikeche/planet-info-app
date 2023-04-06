801c5524ebb212b2ce7380259050d8c577fee659
iraklikeche <irakli.keche@yahoo.com>
2023-04-06 17:19:25 +0400

    Parameter update

--- src/components/MainDisplay.js
+++ src/components/MainDisplay.js
@@ -2,6 +2,7 @@ import React from "react";
 import styled from "styled-components";
 import planet from "../assets/planet-earth.svg";
 import PlanetParametersDetails from "./PlanetParametersDetails";
+import data from "../data.json";
 
 export default function MainDisplay() {
   return (
@@ -10,18 +11,13 @@ export default function MainDisplay() {
         <PlanetImage src={planet} />
       </ImageBox>
       <PlanetDescriptionBox>
-        <NameOfPlanet>PLANET</NameOfPlanet>
-        <PlanetDescription>
-          der text commonly used in the graphic, print, and publishing
-          industries for previewing layouts and visual mockups. LOREM IPSUM
-          GENERATOR Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
-          do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
-          ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-          aliquip ex ea commodo consequat. Duis aute irure dolor in
-          reprehenderit in
-        </PlanetDescription>
+        <NameOfPlanet>{data[2].name}</NameOfPlanet>
+        <PlanetDescription>{data[2].overview.content}</PlanetDescription>
 
-        <span style={{ color: "#fff" }}>SOURCE</span>
+        <span style={{ color: "#fff" }}>
+          Source :{" "}
+          <SourceLink href={data[2].overview.source}>Wikipedia</SourceLink>
+        </span>
       </PlanetDescriptionBox>
       <PlanetParameters>
         <PlanetParametersDetails />
@@ -30,11 +26,19 @@ export default function MainDisplay() {
   );
 }
 
+const SourceLink = styled.a`
+font-size:1.4rem;
+color:#fff;
+opacity:0.5
+font-weight:400;
+`;
+
 const PlanetDescriptionBox = styled.div`
   width: 50%;
   display: flex;
   flex-direction: column;
   margin-left: 15rem;
+  gap: 2.4rem;
   align-items: flex-start;
   justify-content: flex-start;
 `;
@@ -44,12 +48,14 @@ const PlanetDescription = styled.p`
   font-weight: 400;
   color: #fff;
   opacity: 0.5;
+  line-height: 1.5;
 `;
 
 const NameOfPlanet = styled.h2`
   font-size: 6.2rem;
   font-weight: 400;
   color: #fff;
+  text-transform: uppercase;
 `;
 
 const ImageBox = styled.div`
--- src/components/PlanetParametersDetails.js
+++ src/components/PlanetParametersDetails.js
@@ -1,23 +1,49 @@
 import React from "react";
 import styled from "styled-components";
+import data from "../data.json";
 
 export default function PlanetParametersDetails() {
+  const DetailedParameterItem = ({ title, value }) => (
+    <DetailParamBox>
+      <DetailedParameter>
+        <DetailedParametersTitle>
+          {title === "Rotation" || title === "Revolution"
+            ? `${title} time`
+            : title}
+        </DetailedParametersTitle>
+        <DetailedParametersFigures>{value}</DetailedParametersFigures>
+      </DetailedParameter>
+    </DetailParamBox>
+  );
+
   return (
-    <div>
-      <Foo>
-        <DetailedParameter>
-          <DetailedParametersTitle>Rotation Time</DetailedParametersTitle>
-          <DetailedParametersFigures>0.99 Days</DetailedParametersFigures>
-        </DetailedParameter>
-      </Foo>
-    </div>
+    <PlanetParameterDetailsBox>
+      {["Rotation", "Revolution", "Radius", "Temperature"].map(
+        (title, index) => {
+          const key = title.toLowerCase();
+          return (
+            <DetailedParameterItem
+              key={index}
+              title={title}
+              value={data[2][key]}
+            />
+          );
+        }
+      )}
+    </PlanetParameterDetailsBox>
   );
 }
 
-const Foo = styled.div`
+const PlanetParameterDetailsBox = styled.div`
+  display: flex;
+  justify-content: space-evenly;
+`;
+
+const DetailParamBox = styled.div`
   display: inline-block;
-  padding: 2rem;
-  padding-right: 8.4rem;
+  padding: 1.5rem;
+  width: 210px;
+  height: 100px;
   color: #fff;
   border: 1px solid rgba(255, 255, 255, 0.2);
 `;
@@ -36,6 +62,7 @@ const DetailedParametersTitle = styled.span`
   margin-bottom: 1rem;
 `;
 
-const DetailedParametersFigures = styled()`
+const DetailedParametersFigures = styled.span`
   font-size: 2.8rem;
+  color: #fff;
 `;
